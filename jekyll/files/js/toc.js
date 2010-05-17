39c010966f7a83b9f0f701b2c05660a13c5b620d
Ian Barton <ian@manor-farm.org>
2010-05-17 09:01:03 +0100

    Changed scope to show different sections of the document.

--- jekyll/files/js/toc.js
+++ jekyll/files/js/toc.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
 
         $("#toc").append('<p>On this page:</p>')
-            $("div.outline-2 h2").each(function(i) {
+            $("div.outline-2 h2, div.outline-3 h3").each(function(i) {
+                    //if ( $(this).is("h2")) {
                     var current = $(this);
                     current.attr("id", "title" + i);
                     $("#toc").append("<li><a id='link" + i + "' href='#title" +
                                      i + "' title='" + current.attr("tagName") + "'>" + 
         current.html() + "</a>" + "</li>");
+
+                    //}
+
+
 });
-    });
+
+        });
