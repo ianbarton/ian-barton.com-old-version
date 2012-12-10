8510ffb9a9c0185932a236acefc3f73b6bc51fce
Ian Barton <ian@manor-farm.org>
2012-12-10 10:18:24 +0000

    Fix typo in TWITTER_USERNAME.

--- pelican/blog/pelicanconf.py
+++ pelican/blog/pelicanconf.py
@@ -16,8 +16,8 @@ LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
           ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('Twitter', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter', 'http://twitter.com/ianbarton'),
+          ('Github', 'http://github.com/geekinthesticks'),)
 
 DEFAULT_PAGINATION = 10
 
@@ -25,7 +25,7 @@ OUTPUT_PATH = "/srv/http"
 
 STATIC_PATHS = (['images'])
 
-NEWEST_FIRST_ARCHIVES (True)
+#NEWEST_FIRST_ARCHIVES (True)
 
 FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
@@ -36,4 +36,4 @@ ARTICLE_SAVE_AS = "posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html"
 DISQUS_SITENAME = "ianbarton"
 GITHUB_URL = "https://github.com/geekinthesticks"
 GOOGLE_ANALYTICS = "UA-7259094-1"
-TWITTER_USRNAME = 'ianbarton'
+TWITTER_USERNAME = 'ianbarton'
