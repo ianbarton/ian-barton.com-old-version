742f65fad561211fb927cb0240e26cac36a8c433
Ian Barton <ian@manor-farm.org>
2014-01-04 17:53:25 +0000

    Change site url.

--- pelican/blog/pelicanconf.py
+++ pelican/blog/pelicanconf.py
@@ -3,8 +3,8 @@
 
 AUTHOR = u"Ian Barton"
 SITENAME = u"ian-barton.com"
-#SITEURL = 'http://www.ian-barton.com'
-SITEURL = 'http://localhost'
+SITEURL = 'http://www.ian-barton.com'
+#SITEURL = 'http://localhost'
 
 #TEMPLATE_PAGES = {'contents/pages/about.html' : 'pages/about.html'}
 
